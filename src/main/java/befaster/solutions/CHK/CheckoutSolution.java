67a72005f8cf90f8f240b28acbfb411e5f662ead
sbx_user1051 <sbx_user1051@169.254.24.113>
2024-06-16 16:14:48 +0000

    Sun Jun 16 16:14:48 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,21 +6,62 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         int len = skus.length();
         int countA = 0, countB = 0, countC = 0, countD = 0, countE = 0, countF = 0, aux;
-        int prices[]
+        int[] prices = new int[25];
+        int[] counter = new int[25];
         int total = 0;
         for (int i = 0; i < len; i++) {
             if (skus.charAt(i) == 'A') {
-                countA++;
+                counter[0]++;
             } else if (skus.charAt(i) == 'B') {
-                countB++;
+                counter[1]++;
             } else if (skus.charAt(i) == 'C') {
-                countC++;
+                counter[2]++;
             } else if (skus.charAt(i) == 'D') {
-                countD++;
+                counter[3]++;
             } else if (skus.charAt(i) == 'E') {
-                countE++;
+                counter[4]++;
             } else if (skus.charAt(i) == 'F') {
-                countF++;
+                counter[5]++;
+            } else if (skus.charAt(i) == 'G') {
+                counter[6]++;
+            } else if (skus.charAt(i) == 'H') {
+                counter[7]++;
+            } else if (skus.charAt(i) == 'I') {
+                counter[8]++;
+            } else if (skus.charAt(i) == 'J') {
+                counter[9]++;
+            } else if (skus.charAt(i) == 'K') {
+                counter[10]++;
+            } else if (skus.charAt(i) == 'L') {
+                counter[11]++;
+            } else if (skus.charAt(i) == 'M') {
+                counter[12]++;
+            } else if (skus.charAt(i) == 'N') {
+                counter[13]++;
+            } else if (skus.charAt(i) == 'O') {
+                counter[14]++;
+            } else if (skus.charAt(i) == 'P') {
+                counter[15]++;
+            } else if (skus.charAt(i) == 'Q') {
+                counter[16]++;
+            } else if (skus.charAt(i) == 'R') {
+                counter[17]++;
+            } else if (skus.charAt(i) == 'S') {
+                counter[18]++;
+            } else if (skus.charAt(i) == 'T') {
+                counter[19]++;
+            } else if (skus.charAt(i) == 'U') {
+                counter[20]++;
+            } else if (skus.charAt(i) == 'V') {
+                counter[21]++;
+            } else if (skus.charAt(i) == 'W') {
+                counter[22]++;
+            } else if (skus.charAt(i) == 'X') {
+                counter[23]++;
+            } else if (skus.charAt(i) == 'Y') {
+                counter[24]++;
+            } else if (skus.charAt(i) == 'Z') {
+                counter[25]++;
             } else {
                 return -1;
             }
@@ -62,11 +103,14 @@ public class CheckoutSolution {
             }
         }
 
-        total = total + countA * 50 + countB * 30 + countC * 20 + countD * 15 + countE * 40 + countF * 10;
+        for (int i = 0; i < 25; i++) {
+            total += counter[i] * prices[i];
+        }
 
         return total;
     }
 }
+
 
 
 
